eec7a17e0dbd2638257b9296d7e24623822ba376
Ethan Gordon <ekgordon2@gmail.com>
2013-08-01 17:00:48 -0500

    Created basic file structure.

---
+++ BrilliantTest.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+class TribesMan:
+
+	def __init__(self, module, algorithm):
+		
+		# Play Data
+		self.module = module
+		self.algorithm = algorithm
+		
+		# Metrics
+		self.food = 0
+		self.reputation = 0
+		
+		# Game End Condition
+		self.gameOver = False
+
+	def addFood(amount):
+		self.food += amount
+		if food < 1:
+
+
+class HungerGames:
+	def __init__(self):
+
+
+
+def main():
+	
+
+
+if __name__ == '__main__':
+	main()
---
+++ algorithms/slacker.py
@@ -0,0 +1,13 @@
+###### Example form "Sample Code" Section of Competition Website
+
+def hunt_choices(round_number, current_food, current_reputation, m,
+            player_reputations):
+
+    hunt_decisions = ['s' for x in player_reputations]
+    return hunt_decisions
+
+def hunt_outcomes(food_earnings):
+    pass # do nothing
+
+def round_end(award, m, number_hunters):
+    pass # do nothing
